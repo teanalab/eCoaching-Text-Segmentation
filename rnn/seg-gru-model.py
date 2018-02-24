cec4cbd23519dd3a6ebfd0903fa13fd301ab262a
mehediman <mehedi@wayne.edu>
2018-02-23 20:21:48 -0500

    added rnn model for the evaluation of tex-segmentation

--- rnn/seg-gru-model.py
+++ rnn/seg-gru-model.py
@@ -1,68 +1,81 @@
-# GRU with Variable Length Input Sequences to One Character Output
+# RNN with variable length input sequences to one word output
 import numpy
+
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import GRU
+from keras.layers import LSTM
 from keras.utils import np_utils
 from keras.preprocessing.sequence import pad_sequences
+
+import utility
+
+from vocabulary import Vocabulary
 
 # fix random seed for reproducibility
 numpy.random.seed(7)
 
-# define the raw dataset
-alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# create e-coaching dictionary
+vocabulary_obj = Vocabulary()
+vocabulary_obj.create()
 
 # create mapping of characters to integers (0-25) and the reverse
-char_to_int = dict((c, i) for i, c in enumerate(alphabet))
-int_to_char = dict((i, c) for i, c in enumerate(alphabet))
+char_to_int = dict((c, i+1) for i, c in enumerate(vocabulary_obj.vocab))
+int_to_char = dict((i+1, c) for i, c in enumerate(vocabulary_obj.vocab))
 
-# prepare the dataset of input to output pairs encoded as integers
-num_inputs = 1000
-max_len = 5
-dataX = []
-dataY = []
-for i in range(num_inputs):
-	start = numpy.random.randint(len(alphabet)-2)
-	end = numpy.random.randint(start, min(start+max_len,len(alphabet)-1))
-	sequence_in = alphabet[start:end+1]
-	sequence_out = alphabet[end + 1]
-	dataX.append([char_to_int[char] for char in sequence_in])
-	dataY.append(char_to_int[sequence_out])
-	print sequence_in, '->', sequence_out
+# prepare the data set of input to output pairs encoded as integers
+max_len = 10
+folds = 5
+data = utility.get_ecoaching_data()
+folds_data = utility.split_data(data, folds)
+results = []
 
-# convert list of lists to array and pad sequences if needed
-X = pad_sequences(dataX, maxlen=max_len, dtype='float32')
+for fold_num in range(folds):
 
-# reshape X to be [samples, time steps, features]
-X = numpy.reshape(X, (X.shape[0], max_len, 1))
+    train_data = []
+    for i in range(folds):
+        if i != fold_num:
+            train_data = train_data + folds_data[i]
 
-# normalize
-X = X / float(len(alphabet))
+    # get encoded training data
+    X, y = utility.get_encoded_data(train_data, vocabulary_obj, max_len)
 
-# one hot encode the output variable
-y = np_utils.to_categorical(dataY)
+    # reshape X to be [samples, time steps, features]
+    X = numpy.reshape(X, (X.shape[0], max_len, 1))
 
-# create and fit the model
-batch_size = 1
-model = Sequential()
-model.add(GRU(32, input_shape=(X.shape[1], 1)))
-model.add(Dense(y.shape[1], activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(X, y, epochs=50, batch_size=batch_size, verbose=2)
+    # normalize
+    X = X / float(vocabulary_obj.size())
 
-# summarize performance of the model
-scores = model.evaluate(X, y, verbose=0)
-print("Model Accuracy: %.2f%%" % (scores[1]*100))
+    # print total number of instances
+    print("# of instances: ", len(y))
+
+    # create and fit the model
+    batch_size = 16
+    model = Sequential()
+    model.add(LSTM(32, input_shape=(X.shape[1], 1)))
+    # model.add(GRU(32, input_shape=(X.shape[1], 1)))
+    model.add(Dense(y.shape[1], activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.fit(X, y, epochs=5, batch_size=batch_size, verbose=2)
+
+    # summarize performance of the model
+    scores = model.evaluate(X, y, verbose=0)
+    print("\nModel Accuracy: %.2f%%" % (scores[1]*100) + " for fold " + str(fold_num+1) + "\n\n")
+    results.append([fold_num, scores[1]])
+
+# print results
+print("Avg. Accuracy of Model: ")
+print(numpy.mean(results, axis=0))
 
 # demonstrate some model predictions
-for i in range(20):
-	pattern_index = numpy.random.randint(len(dataX))
-	pattern = dataX[pattern_index]
-	x = pad_sequences([pattern], maxlen=max_len, dtype='float32')
-	x = numpy.reshape(x, (1, max_len, 1))
-	x = x / float(len(alphabet))
-	prediction = model.predict(x, verbose=0)
-	index = numpy.argmax(prediction)
-	result = int_to_char[index]
-	seq_in = [int_to_char[value] for value in pattern]
-	print seq_in, "->", result
+# for i in range(20):
+#     pattern_index = numpy.random.randint(len(dataX))
+#     pattern = dataX[pattern_index]
+#     x = pad_sequences([pattern], maxlen=max_len, dtype='float32')
+#     x = numpy.reshape(x, (1, max_len, 1))
+#     x = x / float(vocabulary_obj.size())
+#     prediction = model.predict(x, verbose=0)
+#     index = numpy.argmax(prediction)
+#     result = int_to_char[index]
+#     seq_in = [int_to_char[value] for value in pattern]
+#     print seq_in, "->", result
---
+++ rnn/vocabulary.py
@@ -0,0 +1,58 @@
+import re
+import codecs
+import operator
+
+
+class Vocabulary:
+    def __init__(self, vocab_path='e-coaching.vocab'):
+        self.vocab = dict()
+        self.vocab_path = vocab_path
+
+    # build e-coaching dictionary
+    def build(self):
+        with codecs.open(self.vocab_path, 'r', 'UTF-8') as train_file:
+            words = [x.strip().rstrip('\n') for x in train_file.readlines()]
+            self.vocab = dict((c, i + 1) for i, c in enumerate(words))
+
+        return self
+
+    # read all the data from file and create vocabulary, also find # classes
+    def create(self, data_file='e-coaching_data_stem.txt'):
+        word_dict = {}
+
+        with open(data_file, "r") as file_stream:
+            for line in file_stream:
+                current_line = line.strip().split(",")
+                if len(current_line) < 2:
+                    continue
+
+                words = re.split('(\W+)', self.clean_text(current_line[0]).strip())
+                for word in words:
+                    if len(word.strip()) > 0:
+                        word_dict[word] = 1
+
+        sorted_map = sorted(word_dict.items(), key=operator.itemgetter(0))
+        file_pointer = open(self.vocab_path, "w")
+        for key, val in sorted_map:
+            file_pointer.write(key + "\n")
+        file_pointer.close()
+
+        self.build()
+        return self
+
+    def size(self):
+        return len(self.vocab) + 1
+
+    def tokenize(self, text):
+        return [x.strip() for x in re.split('(\W+)', text) if x.strip()]
+
+    def vectorize(self, text):
+        text = text.lower()
+        words = filter(lambda x: x in self.vocab, self.tokenize(text))
+        return [self.vocab[w] for w in words]
+
+    def clean_text(self, text):
+        new_text = re.sub(r'[^\x00-\x7f]', r'', text)
+        new_text = new_text.replace("\n", "").strip()
+        new_text = re.sub(r'[?|`|>|<|$|.|!|;|:|=|&|\'|\"|\-|/|)|(|\]|[|]', r'', new_text)
+        return new_text
