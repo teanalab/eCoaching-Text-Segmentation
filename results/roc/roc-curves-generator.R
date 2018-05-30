201e578785ea51996bc768a8b3086ce2f3ed14fd
mehediman <mehedi@wayne.edu>
2018-05-29 23:52:07 -0400

    update figure 1 and results for RNNs

--- results/roc/roc-curves-generator.R
+++ results/roc/roc-curves-generator.R
@@ -16,7 +16,7 @@ plot(r1$fpr, r1$tpr, col='red', type="l", ylab = "Sensitivity", xlab = "1-Specif
 lines(r2$fpr, r2$tpr, col='blue', lty=1, lwd=1)
 lines(r3$fpr, r3$tpr, col='green', lty=1, lwd=1)
 lines(0:1, 0:1, col='black', lty=5, lwd=1)
-legend(0.79,0.30, c("KNN", "SVM", "NB"), 
+legend(0.77,0.30, c("KNN", "SVM", "NB"), 
        lty=c(1,1,1), col=c('red', 'blue', 'green'), cex=1, lwd=1)
 
 # print AUC values
@@ -40,6 +40,6 @@ lines(r3$fpr, r3$tpr, col='green', lty=1, lwd=1)
 lines(1-roc_obj_lstm$specificities, roc_obj_lstm$sensitivities, col='cyan', lty=1, lwd=1)
 lines(1-roc_obj_gru$specificities, roc_obj_gru$sensitivities, col='magenta', lty=1, lwd=1)
 lines(0:1, 0:1, col='black', lty=5, lwd=1)
-legend(0.77,0.44, c("KNN", "SVM", "NB", "LSTM", "GRU"), 
+legend(0.75,0.44, c("KNN", "SVM", "NB", "LSTM", "GRU"), 
        lty=c(1,1,1,1,1), col=c('red', 'blue', 'green', 'cyan', 'magenta'), cex=1, lwd=1)
 
