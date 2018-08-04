fd8fb70c00e9be0149d0e83135b36762c8fe6b26
mehediman <mehedi@wayne.edu>
2018-08-03 21:44:50 -0400

    update manuscript

--- plots/embedding-dimension.r
+++ plots/embedding-dimension.r
@@ -12,14 +12,15 @@ wd = '/home/mehedi/teana/projects/eCoaching-Text-Segmentation/plots/'
 data <- read.csv(paste(wd,"parameters/embedding-dimension.csv", sep=''))
 
 # plot graph
-plot(range(50,320), range(0.798, 0.826), type="n", ylab = "F1-Measure", xlab = "vector size")
+plot(range(50,320), range(0.798, 0.826), type="n", ylab = "F1-Measure", xlab = "# of word embedding dimensions")
 lines(data$dim, data$pf1, lwd=1.5, lty=1, type='b', col='green', pch=18)
-points(x = 300, y = 0.825, col = "red", pch = 18, cex = 1, lwd=1.5)
+points(x = 300, y = 0.825, col = "red", pch = 18, cex = 1, lwd=1)
+points(x = 300, y = 0.825, col = "red", pch = 1, cex = 1.5)
 text(300, 0.825, labels = c('0.825'), pos = 1, cex = 0.7)
 textxy(data$dim, data$pf1, labs=data$pf1, pos=3,  cex = 0.7)
 
 lines(data$dim, data$tf1, lwd=1.5, lty=1, type='b', col='blue', pch=17)
-points(x = 300, y = 0.816, col = "blueviolet", pch = 17, cex = 1, lwd=1.5)
+points(x = 300, y = 0.816, col = "blueviolet", pch = 17, cex = 1, lwd=1)
 text(300, 0.816, labels = c('0.816'), pos = 1, cex = 0.7)
 textxy(data$dim, data$tf1, labs=data$tf1, pos=1,  cex = 0.7)
 
