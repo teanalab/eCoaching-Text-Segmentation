16deabfd22d43f52a3baa7cc21887e5c857c520f
mehediman <mehedi@wayne.edu>
2018-08-07 17:53:22 -0400

    update figure 3

--- plots/length-mlp.r
+++ plots/length-mlp.r
@@ -12,7 +12,7 @@ wd = '/home/mehedi/teana/projects/eCoaching-Text-Segmentation/plots/'
 data <- read.csv(paste(wd,"parameters/length.csv", sep=''))
 
 # plot graph
-plot(range(1,7), range(0.605, 0.765), type="n", ylab = "F1-Measure", xlab = "# of next or prior words/punctuation marks in a sample (l)")
+plot(range(1,7), range(0.605, 0.765), type="n", ylab = "F1-Measure", xlab = "# of next or prior words/punctuation marks per instance (l)")
 lines(data$n, data$fmeasure, lwd=1.5, lty=1, type='b', col='blue', pch=18)
 points(x = 2, y = 0.760, col = "red", pch = 1, cex = 2)
 textxy(data$n, data$fmeasure, labs=data$fmeasure, pos=3,  cex = 0.7)
